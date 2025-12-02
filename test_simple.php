6eff942c4d355602b431c6912164e82c28dff312
M.Abdelkhalik <muhmmedabdelkhalik@gmail.com>
2025-12-02 23:49:28 +0200

    chore: remove test file

--- test_simple.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Simple test with one critical issue
-class TestController {
-    public function update($request, $id) {
-        $user = User::find($id);
-        $user->update($request->all()); // Mass assignment vulnerability
-        return response()->json(['success' => true]);
-    }
-}
